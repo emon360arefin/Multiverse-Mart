f8856249253748bd7017ae00f312f651756228b6
Farhasuhi <git config --global user.email suhifarhaislam@gmail.com>
2023-07-26 19:55:32 +0600

    Header 2ndPart

--- multiverse-mart-client/src/Component/Shared/Header/NavLink.jsx
+++ multiverse-mart-client/src/Component/Shared/Header/NavLink.jsx
@@ -6,9 +6,9 @@ const NavLink = ({ category }) => {
 
 
 return (
-    <div className='cursor-pointer text-black py-7 px-3'>
-        <h1><Link>{category.name}</Link></h1>
-        <img src={category.image} alt="" />
+    <div className='cursor-pointer text-black py-7 px-3 flex flex-col justify-center items-center gap-3'>
+        <img className='object-scale-down w-36' src={category.image} alt="" />
+        <h1 className=' font-medium'><Link>{category.name}</Link></h1>
     </div>
 );
 };
