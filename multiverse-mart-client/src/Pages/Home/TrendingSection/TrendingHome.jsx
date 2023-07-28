0f0ef5f0074e425f3ff32f04c19129e3732894fd
Diti Rani Dey <ditidey2017@gmail.com>
2023-07-28 11:37:40 +0100

    trending section

--- multiverse-mart-client/src/Pages/Home/TrendingSection/TrendingHome.jsx
+++ multiverse-mart-client/src/Pages/Home/TrendingSection/TrendingHome.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import SectionTitle from '../../../Component/Shared/Titles/SectionTitle';
 
 const TrendingHome = () => {
     const [shops, setShop] = useState([])
@@ -8,15 +9,37 @@ const TrendingHome = () => {
         fetch('./shop.json')
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                // console.log(data)
                 setShop(data)
             })
     }, [])
+    const colStyle = [
+        'col-span-1 bg-blue-100',
+        'col-span-2 bg-blue-50',
+        'col-span-1 bg-blue-50',
+        'col-span-2 bg-blue-100',
+        'col-span-1 bg-blue-100',
+        'col-span-1 bg-blue-50',
+        'col-span-2 bg-blue-50',
+        'col-span-2 bg-blue-100',
+    ]
+    console.log(shops)
     return (
-        <div>
-            <div className='grid'>
+        <div className='w-[95%] mx-auto my-10 shadow-md'>
+            <SectionTitle title={'Trending Now'}></SectionTitle>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-10 md:p-8'>
                 {
                     // signle card show
+
+                    shops?.slice(0, 8).map((shop, i) => <div key={i} className={`${colStyle[i]} shadow-lg md:h-[500px]`}>
+
+                        <img src={shop.Image} alt="" className='w-full h-2/3'/>
+                        <div className='md:p-5 text-center'>
+                            <h2 className='font-bold'>{shop.Name}</h2>
+                            <p>Price: ${shop.Price}</p>
+                        </div>
+                    </div>)
+
                 }
             </div>
         </div>
